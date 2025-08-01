7990ce9bdbf6fccd3284f4d75771107739f92b4b
Clinton Skakun <clinton.d.skakun@dedupe.ly>
2025-07-31 18:36:17 -0600

    Restored default settings of telescope

--- init.lua
+++ init.lua
@@ -42,16 +42,6 @@ require('lazy').setup({
         require('telescope').setup({
           defaults = {
             path_display = { 'smart' },
-            layout_strategy = "horizontal",
-            layout_config = {
-              horizontal = {
-                prompt_position = "top",
-                width = { padding = 0 },
-                height = { padding = 0 },
-                preview_width = 0.5,
-              },
-            },
-            sorting_strategy = "ascending",
           }
         })
         require('telescope').load_extension('fzf')
