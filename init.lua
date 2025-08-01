a812cd63b842863f1269326a8198d81b2c1e4130
Clinton Skakun <clinton.d.skakun@dedupe.ly>
2025-07-31 20:49:46 -0600

    Added diagnostics display plugin

--- init.lua
+++ init.lua
@@ -17,6 +17,15 @@ vim.opt.rtp:prepend(lazypath)
 -- plugin install
 require('lazy').setup({
   {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      require('tiny-inline-diagnostic').setup()
+      vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end
+  },
+  {
     'nvim-treesitter/nvim-treesitter',
     config = function()
       require('nvim-treesitter.configs').setup({
